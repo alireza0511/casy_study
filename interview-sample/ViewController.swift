70f039583bedf62ed49b4d85db2ee1787bd7874c
Al Khakpour <1khakpour@gmail.com>
2019-09-15 09:24:25 -0500

    first commit

---
+++ interview-sample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  interview-sample
+//
+//  Created by Jazzb Dev on 9/15/19.
+//  Copyright © 2019 Jazzb Dev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
